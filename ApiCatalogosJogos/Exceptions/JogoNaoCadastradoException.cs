07f06959ceed464ea48cc9dedef0b66fea03d468
JPBaraunaDEV <joaopedrorb1@hotmail.com>
2021-11-10 01:12:23 -0300

    Inclusão do catalogo

---
+++ ApiCatalogosJogos/Exceptions/JogoNaoCadastradoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogosJogos.Exceptions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException()
+            :base("JOGO AINDA NÃO CADASTRADO!")
+        { }
+    }
+}
---
+++ ApiCatalogosJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogosJogos.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O NOME do jogo deve ter entre 3 a 100 caracteres")]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O NOME DA PRODUTORA deve ter entre 3 a 100 caracteres")]
+        public string Produtora { get; set; }
+        [Required]
+        [Range(1, 1000, ErrorMessage = "O PREÇO deve ser de $1 a $1000")]
+        public double Preco { get; set; }
+    }
+}
